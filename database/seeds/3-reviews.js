cb1c0ee832aafd935882323e8c88a61412cb5f17
Chase Hooper <Hoopchaser@gmail.com>
2020-01-06 14:40:38 -0700

    changed into to string

--- database/seeds/3-reviews.js
+++ database/seeds/3-reviews.js
@@ -2,6 +2,6 @@ exports.seed = function(knex) {
   // Deletes ALL existing entries
       // Inserts seed entries
       return knex('reviews').insert([
-        { restaurant_id: 1, typeofcusine: "Mexican-American", menuitem: "Beefy Frito Burrito", photourl: "https://www.tacobell.com/images/22169_beefy_crunch_burrito_269x269.jpg", price: 1.99, itemrating: 4, itemreview: "it was a crunchy taco that was good." }
+        { restaurant_id: 1, typeofcusine: "Mexican-American", menuitem: "Beefy Frito Burrito", photourl: "https://www.tacobell.com/images/22169_beefy_crunch_burrito_269x269.jpg", price: "$1.99", itemrating: 4, itemreview: "it was a crunchy taco that was good." }
       ]);
 };
