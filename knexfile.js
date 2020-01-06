3da9934c1fb231e8d33e8c97c9db9adaddcd8a70
Chase Hooper <Hoopchaser@gmail.com>
2020-01-06 14:26:15 -0700

    maybe fix?

--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,11 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
     migrations: { directory: "./database/migrations" },
     seeds: { directory: "./database/seeds" }
   }
