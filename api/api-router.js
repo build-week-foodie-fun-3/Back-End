ae29ee5b3dc8f8f7011bc4eaf2c3574409a69311
Chase Hooper <Hoopchaser@gmail.com>
2020-01-07 10:29:10 -0700

    made location not unique

--- api/api-router.js
+++ api/api-router.js
@@ -8,13 +8,12 @@ router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
-
   Users.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      res.status(201).json({ message: "User Created", saved });
     })
     .catch(error => {
-      res.status(500).json(error);
+      res.status(500).json({ message: "User Failed to create", error });
     });
 });
 
